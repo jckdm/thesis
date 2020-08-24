9cc32bd1b6c8565870ab57bd7e53b5c3d1019191
Jack Adam <jack.adam@yale.edu>
2020-08-24 14:16:45 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function() {
+	var w = $('.content h2');
+	for (var i = 0; i < w.length; i++) {
+    var h = $(w[i]).html();
+		$(w[i]).attr('id', h);
+    $('#nav')[0].append('<a href="#' + h + '">' + h + '</a> <br>');
+	}
+
+	$(window).scroll(function() {
+		var s = $(document).scrollTop();
+    var win = $(window).width();
+    var w = 900 / win;
+    if (win < 415) { w = w / 1.65; }
+
+		if (s < (800 * w)) {
+			$('#pic').attr('src', 'img/1.jpg');
+		}
+		if (s > (800 * w) && s < (1500 * w)) {
+			$('#pic').attr('src', 'img/2.jpg');
+		}
+		if (s > (1500 * w)) {
+			$('#pic').attr('src', 'img/3.jpg');
+		}
+	})
+})
