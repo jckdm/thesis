c30dd8cc3510f1105ec4e3356f7632b6227f1d81
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-08-27 00:06:50 -0400

    redid type weights, typographic hierarchy

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 $(function() {
-	var w = $('.content h2');
+	var w = $('.content text');
 	var n = $('#nav')[0];
 	for (var i = 0; i < w.length; i++) {
     var h = $(w[i]).html();
 		$(w[i]).attr('id', h);
     $(n).append('<a target="_self" href="#' + h + '">' + h + '</a> <br>');
 	}
+	$(n).append('<br> <a id="sub" href="https://jackadam.cc" target="_blank">JACK ADAM</a> <br> <a href="https://github.com/jckdm/thesis" target="_blank">readme.md</a>');
 
-	var observer = new IntersectionObserver(callback);
+	var options = { threshold: 0.5 }
+	var observer = new IntersectionObserver(callback, options);
 
-	var targets = ['isaias', 'atop', 'two'];
+	var targets = ['isaias', 'atop'];
 	for (var i = 0; i < targets.length; i++) {
 		observer.observe(document.querySelector('#' + targets[i]));
 	}
@@ -21,7 +23,6 @@ function callback(entries) {
 
 	if (e.isIntersecting) {
 		if (e.target.id == 'isaias') { $(p).attr('src', 'img/1.jpg'); }
-		if (e.target.id == 'atop') { $(p).attr('src', 'img/2.jpg'); }
-		if (e.target.id == 'two') { $(p).attr('src', 'img/3.jpg'); }
+		else if (e.target.id == 'atop') { $(p).attr('src', 'img/2.jpg'); }
 	}
 }
