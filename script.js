675e36f88239f3f170203e42c191011467015778
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-08-28 16:59:21 -0400

    t y p o g r a p h i e

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
 	var w = $('.content text');
-	var n = $('#nav')[0];
+	var n = $('#navlinks')[0];
 	for (var i = 0; i < w.length; i++) {
     var h = $(w[i]).html();
 		$(w[i]).attr('id', h);
@@ -11,7 +11,7 @@ $(function() {
 	var options = { threshold: 0.5 }
 	var observer = new IntersectionObserver(callback, options);
 
-	var targets = ['isaias', 'atop'];
+	var targets = ['isaias', 'atop', 'tweed'];
 	for (var i = 0; i < targets.length; i++) {
 		observer.observe(document.querySelector('#' + targets[i]));
 	}
@@ -21,8 +21,11 @@ function callback(entries) {
 	var p = $('#pic');
 	var e = entries[0];
 
-	if (e.isIntersecting) {
-		if (e.target.id == 'isaias') { $(p).attr('src', 'img/1.jpg'); }
-		else if (e.target.id == 'atop') { $(p).attr('src', 'img/2.jpg'); }
-	}
+	var pics = {
+	  isaias: '1.jpg',
+	  atop: '2.jpg',
+	  tweed: '3.gif'
+	};
+	
+	if (e.isIntersecting) { $(p).attr('src', 'img/' + pics[e.target.id]); }
 }
