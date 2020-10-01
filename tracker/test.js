88fa84f693101204c3ca50f901a17950a385e6ba
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-01 00:35:22 -0400

    added toggles for each app

--- tracker/test.js
+++ tracker/test.js
@@ -7,13 +7,11 @@ var w = parseInt(fs[1]);
 var h = parseInt(fs[2]);
 
 var padding = 30;
-
 var apps = [];
 var uniqueApps = [];
 var coords = [];
 var dates = [];
 var times = [];
-
 var color = {};
 
 function colorize() {
@@ -26,12 +24,51 @@ function colorize() {
 }
 
 d3.select("input")
-    .on("input", function() {
-        var radius = +d3.select(this).node().value;
-        d3.selectAll("circle")
-            .attr("r", radius);
-        $('#rad').text('r = ' + radius);
+  .on("input", function() {
+      var radius = +d3.select(this).node().value;
+      d3.selectAll("circle")
+          .attr("r", radius);
+      $('#rad').text('r = ' + radius);
 });
+
+// d3.selectAll('circle')
+//     .attr('fill', 'none')
+//     .attr('stroke', 'none');
+
+function query(id) {
+  var cId = id.replace(/\W/g, '');
+  var e = document.getElementById(id);
+
+  if (id == 'all') {
+    for (var i = 1; i < e.parentElement.length; i++) {
+        query(e.parentElement[i].id);
+    }
+
+    if (e.style.color == 'white') {
+      e.style.backgroundColor = 'white';
+      e.style.color = 'black';
+    }
+    else {
+      e.style.backgroundColor = 'black';
+      e.style.color = 'white';
+    }
+  }
+
+  else if (e.style.color == 'white') {
+    e.style.backgroundColor = color[cId];
+    e.style.color = 'black';
+    d3.selectAll('circle.' + cId)
+        .attr('fill', function(d, i) { return color[cId]; })
+        .attr('stroke', 'gray');
+  }
+  else {
+    e.style.backgroundColor = 'black';
+    e.style.color = 'white';
+    d3.selectAll('circle.' + cId)
+        .attr('fill', 'none')
+        .attr('stroke', 'none');
+  }
+}
 
 var c = 0;
 
@@ -41,17 +78,17 @@ Papa.parse(filename, {
     skipEmptyLines: true,
   	step: function(row) {
       var app = row['data']['app'];
-      if (!apps.includes(app)) {
+      if (!apps.includes(app.replace(/\W/g, ''))) {
         uniqueApps.push(app);
         if (c < 12) {
-          color[app] = d3.schemeSet3[c];
+          color[app.replace(/\W/g, '')] = d3.schemeSet3[c];
           c++;
         }
         else if (c == 12) {
-          color[app] = colorize();
+          color[app.replace(/\W/g, '')] = colorize();
         }
       }
-      apps.push(app);
+      apps.push(app.replace(/\W/g, ''));
       coords.push([parseFloat(row['data']['x']), parseFloat(row['data']['y'])]);
       dates.push(row['data']['date']);
       times.push(row['data']['time']);
@@ -59,7 +96,7 @@ Papa.parse(filename, {
   	complete: function() {
 
       for (var i = 0; i < uniqueApps.length; i++) {
-        $('#form').append('<input type="checkbox" checked="true" id="' + uniqueApps[i] + '"> <label for"' + uniqueApps[i] + '">' + uniqueApps[i] + '</label> &nbsp;');
+        $('#form').append('<button type="button" style="background-color: ' + color[uniqueApps[i].replace(/\W/g, '')] + ';" onclick="query($(this)[0].id)" id="' + uniqueApps[i] + '">' + uniqueApps[i] + '</button>');
       }
 
       var svg = d3.select('body')
@@ -102,6 +139,7 @@ Papa.parse(filename, {
           .append('circle')
           .attr('cx', function(d) { return xScale(d[0]); })
           .attr('cy', function(d) { return yScale(h - d[1]); })
+          .attr('class', function(d, i) { return apps[i].replace(/\W/g, ''); })
           .attr('stroke', 'gray')
           .attr('stroke-width', '1')
           .attr('fill', function(d, i) { return color[apps[i]]; })
