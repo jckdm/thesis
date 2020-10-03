b74585d063f2359ce893b1953d234121c54ef039
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-03 18:51:15 -0400

    styled and designed code. more functions, more fun. properly add/remove lines.

--- tracker/trackerHelper.js
+++ tracker/trackerHelper.js
@@ -1,10 +1,4 @@
-var fs = filename.split('-');
-
-$('#title').text(fs[0]);
-
-var w = parseInt(fs[1]);
-var h = parseInt(fs[2]);
-
+var c = 0;
 var padding = 30;
 var apps = [];
 var uniqueApps = [];
@@ -14,7 +8,6 @@ var times = [];
 var color = {};
 var timeFlag = false;
 var lineFlag = false;
-var c = 0;
 
 var scheme = ['rgb(141,211,199)', 'rgb(255,255,179)', 'rgb(190,186,218)',
 'rgb(251,128,114)', 'rgb(128,177,211)', 'rgb(253,180,98)', 'rgb(179,222,105)',
@@ -27,18 +20,20 @@ function colorize() { return 'rgb(' + getR(25, 230) + ',' + getR(25, 230) + ',' 
 
 d3.select('input')
   .on('input', function() {
-      var radius = +d3.select(this).node().value;
-      d3.selectAll('circle')
-        .attr('r', radius);
-      $('#rad').text('r = ' + radius);
+    var radius = +d3.select(this).node().value;
+    d3.selectAll('circle').attr('r', radius);
+    $('#rad').text('r = ' + radius);
 });
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 function filters(id) {
   swap(id, id, document.getElementById(id), false);
-  if (id == 'timesort') { timeFlag = (timeFlag == false) ? true : false; }
-  else if (id == 'linesort') { lineFlag = (lineFlag == false) ? true : false; }
+  if (id == 'timesort') { timeFlag = !timeFlag; }
+  else if (id == 'linesort') {
+    if (!lineFlag && !timeFlag) { filters('timesort'); }
+    lineFlag = !lineFlag;
+  }
 }
 
 function swap(id, cId, e, flag) {
@@ -56,6 +51,23 @@ function swap(id, cId, e, flag) {
   }
 }
 
+function circleSwap(cId, on) {
+  var cc = (on) ? color[cId] : 'none';
+  var stroke = (on) ? 'gray' : 'none';
+
+  if (lineFlag) { d3.selectAll('g.L').remove(); }
+  if (timeFlag) {
+    var circles = $('circle.' + cId);
+    var num = circles.length;
+    for (var i = 0; i < num; i++) {
+      var curr = circles[i].style;
+      curr.fill = 'none';
+      curr.stroke = 'none';
+    }
+  }
+  else { d3.selectAll('circle.' + cId).attr('fill', cc).attr('stroke', stroke); }
+}
+
 async function query(id) {
   var e = document.getElementById(id);
 
@@ -63,12 +75,11 @@ async function query(id) {
     var cId = id.replace(/\W/g, '');
 
     if (id == 'invert') {
-      for (var i = 1; i < e.parentElement.length; i++) {
-        query(e.parentElement[i].id);
-      }
+      for (var i = 1; i < e.parentElement.length; i++) { query(e.parentElement[i].id); }
       swap(id, cId, e, false);
     }
 
+    // off -> on
     else if (e.style.color == 'white') {
       swap(id, cId, e, true);
 
@@ -80,30 +91,31 @@ async function query(id) {
           circles[i].style.fill = color[cId];
 
           if (lineFlag) {
-            var gL = d3.select('svg').append('g').attr('id','L');
+            var gL = d3.select('svg').append('g').attr('class','L');
+
+            var curr = circles[i].attributes;
+            var next = circles[i+1].attributes;
+
             gL.append('line')
               .style('stroke', color[cId])
               .style('stroke-width', 1)
-              .attr('x1', parseFloat(circles[i].attributes.cx.value))
-              .attr('y1', parseFloat(circles[i].attributes.cy.value))
-              .attr('x2', parseFloat(circles[i+1].attributes.cx.value))
-              .attr('y2', parseFloat(circles[i+1].attributes.cy.value));
+              .attr('class', cId)
+              .attr('x1', parseFloat(curr.cx.value))
+              .attr('y1', parseFloat(curr.cy.value))
+              .attr('x2', parseFloat(next.cx.value))
+              .attr('y2', parseFloat(next.cy.value));
           }
-
           await sleep(num / 1000);
         }
+        // append last circle
+        circles[num - 1].style.fill = color[cId];
       }
-      else {
-        d3.selectAll('circle.' + cId)
-          .attr('fill', function(d, i) { return color[cId]; })
-          .attr('stroke', 'gray');
-      }
+      else { circleSwap(cId, true); }
     }
+    // on -> off
     else {
       swap(id, cId, e, false);
-      d3.selectAll('circle.' + cId)
-        .attr('fill', 'none')
-        .attr('stroke', 'none');
+      circleSwap(cId, false);
     }
   }
 }
