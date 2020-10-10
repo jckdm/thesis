62b2fdd688c5c2a693a8eae18aa7b93601b065a6
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-10 01:13:21 -0400

    updated js to use =>. mis-labeled blog post. lol.

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
-$(function() {
-	var w = $('#content text');
-	var n = $('#navlinks')[0];
-	for (var i = 0; i < w.length; i++) {
-    var h = $(w[i]).html();
+$(() => {
+	const w = $('#content text');
+	const n = $('#navlinks')[0];
+	for (let i = 0; i < w.length; i++) {
+    const h = $(w[i]).html();
 		$(w[i]).attr('id', h);
     $(n).append('<a target="_self" href="#' + h + '">' + h + '</a> <br>');
 	}
--- js/story.js
+++ js/story.js
@@ -1,20 +1,20 @@
-$(function() {
-	var options = { threshold: 0.5 }
-	var observer = new IntersectionObserver(callback, options);
+$(() => {
+	const options = { threshold: 0.5 }
+	const observer = new IntersectionObserver(callback, options);
 
-	var targets = ['isaias', 'atop', 'tweed', 'boundaries'];
-	for (var i = 0; i < targets.length; i++) {
+	const targets = ['isaias', 'atop', 'tweed', 'boundaries'];
+	for (let i = 0; i < targets.length; i++) {
 		observer.observe(document.querySelector('#' + targets[i]));
 	}
 })
 
-function callback(entries) {
-	var pics = {
+callback = (entries) => {
+	const pics = {
 	  isaias: '1.jpg',
 	  atop: '2.jpg',
 	  tweed: '3.gif',
 		boundaries: '4.jpg'
 	};
-	var e = entries[0];
+	const e = entries[0];
 	if (e.isIntersecting) { $('#pic').attr('src', 'img/' + pics[e.target.id]); }
 }
