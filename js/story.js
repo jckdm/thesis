c56d1b73924d1c874908c87c66cc00b1a5394a8c
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-19 00:16:13 -0400

    new blog post. fuck jeff bezos. shoutout to sam & trey.

--- js/story.js
+++ js/story.js
@@ -2,7 +2,7 @@ $(() => {
 	const options = { threshold: 0.5 }
 	const observer = new IntersectionObserver(callback, options);
 
-	const targets = ['isaias', 'atop', 'tweed', 'boundaries', 'visibility', 'hallway'];
+	const targets = ['isaias', 'atop', 'tweed', 'boundaries', 'visibility', 'hallway', 'monitor'];
 	for (let i = 0; i < targets.length; i++) {
 		observer.observe(document.querySelector('#' + targets[i]));
 	}
@@ -15,7 +15,8 @@ callback = (entries) => {
 	  tweed: '3.gif',
 		boundaries: '4.jpg',
 		visibility: '5.gif',
-		hallway: '6.jpg'
+		hallway: '6.jpg',
+		monitor: '7.jpg'
 	};
 	const e = entries[0];
 	if (e.isIntersecting) { $('#pic').attr('src', 'img/' + pics[e.target.id]); }
