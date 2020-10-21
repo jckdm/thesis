59eecb0389bedd6478b61bccaf021a155558c7cf
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-21 00:59:56 -0400

    new ramble / blog post. good fucking night.

--- js/story.js
+++ js/story.js
@@ -2,7 +2,7 @@ $(() => {
 	const options = { threshold: 0.5 }
 	const observer = new IntersectionObserver(callback, options);
 
-	const targets = ['isaias', 'atop', 'tweed', 'boundaries', 'visibility', 'hallway', 'monitor'];
+	const targets = ['isaias', 'atop', 'tweed', 'boundaries', 'visibility', 'hallway', 'monitor', 'beach'];
 	for (let i = 0; i < targets.length; i++) {
 		observer.observe(document.querySelector('#' + targets[i]));
 	}
@@ -16,7 +16,8 @@ callback = (entries) => {
 		boundaries: '4.jpg',
 		visibility: '5.gif',
 		hallway: '6.jpg',
-		monitor: '7.jpg'
+		monitor: '7.jpg',
+		beach: '8.jpg'
 	};
 	const e = entries[0];
 	if (e.isIntersecting) { $('#pic').attr('src', 'img/' + pics[e.target.id]); }
