1ceb4e0ae567ee02af6839c93339e373e92ac2a1
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-21 13:06:46 -0400

    add toggle visibility of zero-value squares in heatmap.

--- js/mapper-helper.js
+++ js/mapper-helper.js
@@ -4,6 +4,7 @@ const dates = [];
 const times = [];
 
 let g = false;
+let zero = 'black';
 let box = 1;
 let sq = 10;
 let max = -1;
@@ -11,7 +12,7 @@ let max = -1;
 // calculate color as percentage of max
 scaleColor = (c) => {
   let p = 255.0 * (c / box);
-  return 'rgb(' + p + ',' + p + ',' + p + ')';
+  return (p == 0.0) ? zero : 'rgb(' + p + ',' + p + ',' + p + ')';
 }
 
 // pause for ms millseconds
@@ -36,11 +37,7 @@ grid = (x) => {
   for (let row = 0; row < h / sq; row++) {
     data.push([]);
     for (let column = 0; column < w / sq; column++) {
-      data[row].push({
-        x: xPos,
-        y: yPos,
-        c: 0
-      })
+      data[row].push({ x: xPos, y: yPos, c: 0 })
       xPos += sq;
     }
     xPos = 1;
@@ -57,15 +54,14 @@ grid = (x) => {
   // if scaling, divide by max. if not, divide by 1
   box = ($('#scaled')[0].checked) ? max : 1;
 
+  // switch btw filled and unfilled 0 value squares
+  zero = ($('#hide')[0].checked) ? 'none' : 'black';
+
   const gg = d3.select('#grid')
-    .append('svg')
-    .attr('width', w + padding)
-    .attr('height', h + padding);
+    .append('svg').attr('width', w + padding).attr('height', h + padding);
 
   const row = gg.selectAll('.row')
-    .data(data)
-    .enter()
-    .append('g');
+    .data(data).enter().append('g');
 
   row.selectAll('.square')
     .data((d) => d)
@@ -79,14 +75,20 @@ grid = (x) => {
     .style('stroke', '#262626')
     // on hover, fill green and display seconds
     .on('mouseover', function(d) {
-      $('#sec').text(Object.values($(this)[0])[0].c + ' seconds')
-      $(this)[0].style.fill = '#3CB371';
+      let val = Object.values($(this)[0])[0].c;
+      // only show values on non-zero / visible squares
+      if (zero == 'black' || val != 0) {
+        $('#sec').text(val + ' seconds')
+        $(this)[0].style.fill = '#3CB371';
+      }
     })
     .on('mouseout', function(d) {
-      $('#sec').text('');
-      $(this)[0].style.fill = scaleColor(Object.values($(this)[0])[0].c);
+      let val = Object.values($(this)[0])[0].c;
+      if (zero == 'black' || val != 0) {
+        $('#sec').text('');
+        $(this)[0].style.fill = scaleColor(Object.values($(this)[0])[0].c);
+      }
     });
-
     // enjoy your map
     g = true;
 }
