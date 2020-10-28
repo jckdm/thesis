2072941bc4cb96fbc3f488f41792f686c94cdc0e
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-10-28 15:45:19 -0400

    rewrote reader in JS. rm -reader.csv. construct viz.html dynamically with JS.

--- js/reader.js
+++ js/reader.js
@@ -1,8 +1,7 @@
 // color counter
 let c = 0;
-
-const data = [];
 const color = {};
+let curr, last;
 
 Papa.parse(filename, {
     download: true,
@@ -11,26 +10,25 @@ Papa.parse(filename, {
   	step(row) {
       const r = row.data;
       const app = r.app;
-      const cleanApp = app.replace(/\W/g, '');
-
-      if (!color[cleanApp]) {
-        // get next color in scheme
-        if (c < 17) {
-          color[cleanApp] = scheme[c];
-          c++;
+      // if app recorded
+      if (app) {
+        const cleanApp = app.replace(/\W/g, '');
+        // if not in dictionary
+        if (!color[cleanApp]) {
+          // get next color in scheme
+          if (c < 17) {
+            color[cleanApp] = scheme[c];
+            c++;
+          }
+          // or generate a new one
+          else { color[cleanApp] = colorize(); }
         }
-        // or generate a new one
-        else { color[cleanApp] = colorize(); }
+        curr = cleanApp;
+        // if app switched
+        if (curr != last) {
+          $('body').append('<p style="color:' + color[cleanApp] + ';">' + app + ' <span>' + r.time + '</span></p>');
+          last = curr;
+        }
       }
-
-      // push data to array
-      data.push([app, r.time]);
-  	},
-  	complete: () => {
-      for (let i = 0; i < data.length; i++) {
-        let curr = data[i];
-        let app = curr[0];
-        $('body').append('<p style="color:' + color[app.replace(/\W/g, '')] + ';">' + app + ' <span>' + curr[1] + '</span></p>');
-      }
-    }
+  	}
 });
--- js/viz.js
+++ js/viz.js
@@ -1,5 +1,28 @@
+viz = (s) => window.open(s[0].innerText.toLowerCase() + '.html?file=' + s[0].parentNode.firstChild.data.trim() + '.csv', '_blank');
 choice = (s) => window.open('data/tracer/' + s[0].parentNode.firstChild.data.trim().split(' ').join('') + '-' + s[0].innerText + '.svg', '_blank');
-graph = (s) => window.open('grapher.html?file=' + s[0].parentNode.firstChild.data.trim() + '.csv', '_blank');
-map = (s) => window.open('mapper.html?file=' + s[0].parentNode.firstChild.data.trim() + '.csv', '_blank');
-read = (s) => window.open('reader.html?file=' + s[0].parentNode.firstChild.data.trim() + s[0].innerText.toLowerCase() + '-.csv', '_blank');
-space = (s) => window.open('spacer.html?file=' + s[0].parentNode.firstChild.data.trim() + '.csv', '_blank');
+
+$(() => {
+  // get all content elements
+  const c = $('#content')[0].children;
+  let vizFlag = false;
+
+  // loop over viz and images
+  for (let i = 1; i < c.length - 1; i++) {
+    const name = c[i].nodeName;
+
+    // if at TRACER, switch to images
+    if (name == 'TEXT') { vizFlag = true; }
+
+    // if TRACKER
+    else if (name == 'P' && !vizFlag) {
+      c[i].innerHTML += ' &nbsp; &nbsp; <span class="viz">Grapher</span> &nbsp; &nbsp;<span class="viz">Reader</span> &nbsp; &nbsp;<span class="viz">Spacer</span> &nbsp; &nbsp;<span class="viz">Mapper</span>';
+    }
+    // if TRACER
+    else if (name == 'P') {
+      c[i].innerHTML += ' &nbsp; &nbsp; <span class="img">Unsorted</span> &nbsp; &nbsp;<span class="img">Sorted</span> &nbsp; &nbsp;<span class="img">Overlaid</span>';
+    }
+  }
+  // add onclick functions to each
+  $('.viz').on('click', function() { viz($(this)); });
+  $('.img').on('click', function() { choice($(this)); });
+});
