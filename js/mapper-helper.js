ed86065cf54bd3421953c0d100ff91329d566e16
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-11-04 12:38:31 -0500

    test

--- js/mapper-helper.js
+++ js/mapper-helper.js
@@ -7,6 +7,10 @@ let zero = 'black';
 let sq = 10;
 let max = -1;
 
+($(() => {
+  $('#size').click(() => { alert("submitted"); })
+}))
+
 // calculate color as percentage of max
 scaleColor = (c) => {
   let p = 255.0 * (Math.log(c) / Math.log(max));
