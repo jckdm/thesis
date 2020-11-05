611391c3610cef7dfd44c286ffad2aed8394271a
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-11-05 11:40:36 -0500

    add tooltip reader. change size of rect on screen size change.

--- js/reader-helper.js
+++ js/reader-helper.js
@@ -12,22 +12,31 @@ const attrs = {x: 20, height: 15, width: 200, stroke: '#262626', 'stroke-width':
 let allcolor = false;
 let alltext = false;
 
+// on resize, redraw rectangles but don't flip flag
+$(window).resize(() => showcolor(0) );
+
 resize = (h) => {
+  // get all rectangles
   let rects = $('#path')[0].children;
+  // calc change in height
   let change = height - h;
+  // reset y to top
   y = 0;
 
+  // change height and y for each rect
   for (r of rects) {
     r.setAttribute('height', h);
     r.setAttribute('y', y);
     y += h;
     $('#path').attr('height', y);
   }
+  // update at end
   height = h;
 }
 
 showtext = (x) => {
   if (x) {
+    // clear text
     $('#reader').html('');
     alltext = !alltext;
   }
@@ -38,6 +47,7 @@ showtext = (x) => {
     curr = apps[i];
     // if app switched
     if (curr != last) {
+      // append next app
       $('#reader').append('<text style="color:' + colors[curr.replace(/\W/g, '')] + ';">' + curr + ' <span>' + times[i] + '</span></text><br>');
       last = (alltext) ? '' : curr;
     }
@@ -45,18 +55,22 @@ showtext = (x) => {
 }
 
 showcolor = (x) => {
-  if (x) {
+  if (x || x == 0) {
     // remove svg, append fresh onoe
     $('#path').remove();
     $('#box').append('<svg id="path"></svg>');
-    allcolor = !allcolor;
   }
+  // only switch on click, not on resize
+  if (x) { allcolor = !allcolor; }
 
   // reset y coord
   y = 0;
   // update height
   attrs['height'] = height;
-  attrs['width'] = $(window).width() / 3.5;
+  // scalable width
+  const newW = $(window).width() / 2.5;
+  attrs['width'] = newW;
+  $('#path').attr('width', newW);
   // reset last
   last = '';
 
@@ -81,11 +95,35 @@ showcolor = (x) => {
       }
     }
 
-  $('rect').mouseenter(function() {
-    $(this).css({'stroke': 'white', 'stroke-width': 2.5});
-    $('#app').text($(this)[0].attributes['class'].value);
-  }).mouseout(function(){
-    $(this).css({'stroke': '#262626', 'stroke-width': 0.125});
-    $('#app').html('&nbsp;');
-  })
+    // append tooltip
+    d3.select('body')
+      .append('div')
+      .attr('id', 'tooltip');
+
+    // select all rects
+    d3.selectAll('rect')
+      .on('mouseover', function() {
+        // border on hover
+        $(this).css({'stroke': 'white', 'stroke-width': 2.5});
+        // show and add class as text
+        d3.select('#tooltip')
+          .text($(this)[0].attributes['class'].value)
+          .transition()
+          .duration(100)
+          .style('visibility', 'visible')
+      })
+      .on('mouseout', function() {
+        $(this).css({'stroke': '#262626', 'stroke-width': 0.125});
+        // hide tooltip
+        d3.select('#tooltip')
+          .transition()
+          .duration(100)
+          .style('visibility', 'hidden')
+      })
+      // update position on move
+      .on('mousemove', () => {
+        d3.select('#tooltip')
+          .style('left', event.pageX + 30 + 'px')
+          .style('top', event.pageY + 'px')
+      })
 }
