065a5ed62c55fbd7f6e0e3fa59cb80b989dcb81b
Jack Adam <jackadam@Jacks-MacBook-Pro-3.local>
2020-11-08 00:55:17 -0500

    new blog post. how about biden's speech tonight, huh

--- js/story.js
+++ js/story.js
@@ -2,7 +2,7 @@ $(() => {
 	const options = { threshold: 0.5 }
 	const observer = new IntersectionObserver(callback, options);
 
-	const targets = ['isaias', 'atop', 'tweed', 'boundaries', 'visibility', 'hallway', 'monitor', 'beach', 'ballot'];
+	const targets = ['isaias', 'atop', 'tweed', 'boundaries', 'visibility', 'hallway', 'monitor', 'footprints', 'ballot', 'wifi'];
 	for (t of targets) { observer.observe(document.querySelector('#' + t)); }
 })
 
@@ -15,8 +15,9 @@ callback = (entries) => {
 		visibility: '5.gif',
 		hallway: '6.jpg',
 		monitor: '7.jpg',
-		beach: '8.jpg',
-		ballot: '9.jpg'
+		footprints: '8.jpg',
+		ballot: '9.jpg',
+		wifi: '10.jpg'
 	};
 	const e = entries[0];
 	if (e.isIntersecting) { $('#pic').attr('src', 'img/' + pics[e.target.id]); }
